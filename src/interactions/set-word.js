46d8aff9abe8bcc457c9b92fbba26f61dd79d28a
Houcinos <houcinos.dhieb@gmail.com>
2020-12-03 01:30:22 +0100

    Here are my 4 files interaction: display words and set words, Logic: repeat characters, Renders; describe word

--- src/interactions/set-word.js
+++ src/interactions/set-word.js
@@ -8,4 +8,17 @@ console.log('--- loading interaction -->');
  * if there is a saved word, the user is asked to confirm replacing it.
  * if the user confirms, they enter a new word that replaces the old one.
  */
-const setWord = () => {};
+const setWord = () => {
+    if (savedword === ""){
+      alert("you have no saved word");
+      return;
+    }
+    confirmMessage = "this is your word" + savedword + "do you want to replace it?"
+    userConfirm = confirm(confirmMessage);
+    if (userConfirm){
+      enterWord(message = "enter the replace word");
+    }
+    else {
+      savedword = savedword;
+    }
+  };
