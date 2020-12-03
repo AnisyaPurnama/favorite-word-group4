a7fe4c0bc750f123e37995897b1f1be608730bf2
Mahmoud Abu Azoum <krakla@gmail.com>
2020-12-03 19:03:54 +0100

    updated

--- src/logic/remove-characters.js
+++ src/logic/remove-characters.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-console.log('--- loading logic --> ');
+console.log("--- loading logic --> ");
 
 /**
  * removes all the provided characters from a string
@@ -8,23 +8,37 @@ console.log('--- loading logic --> ');
  * @param {string} [charactersToRemove=''] - string of characters to remove
  * @returns {string} new string with all provided characters removed
  */
-const removeCharacters = () => {};
+
+
+const removeCharacters = (text = '', charactersToRemove = '') => {
+  if (charactersToRemove !== "") {
+    let newString = '';
+    for (let index = 0; index < charactersToRemove.length; index++){
+      newString = text.replace(charactersToRemove[index], '')
+      text = newString;
+      }
+      console.log(newString)
+      return newString;
+  }
+  console.log(text);
+  return text ;
+};
 
 {
   const consoleLog = console.log;
   console.log = () => {};
 
   try {
-    console.assert(removeCharacters('asdf') === 'asdf', 'Test 0 a');
-    console.assert(removeCharacters('') === '', 'Test 0 b');
-    console.assert(removeCharacters('asdf', 'ad') === 'sf', 'Test 1');
-    console.assert(removeCharacters('ASDF', 'ad') === 'ASDF', 'Test 2');
-    console.assert(removeCharacters('', 'ad') === '', 'Test 3');
-    console.assert(removeCharacters('asdf', 'xz') === 'asdf', 'Test 4');
-    console.assert(removeCharacters('asdf', '') === 'asdf', 'Test 5');
-    console.assert(removeCharacters('howdy', 'dw') === 'hoy', 'Test 6');
-    console.assert(removeCharacters('AaSsDdFf', 'aSdF') === 'AsDf', 'Test 7');
-    console.assert(removeCharacters('asdf', 'fdsa') === '', 'Test 8');
+    console.assert(removeCharacters("asdf") === "asdf", "Test 0 a");
+    console.assert(removeCharacters("") === "", "Test 0 b");
+    console.assert(removeCharacters("asdf", "ad") === "sf", "Test 1");
+    console.assert(removeCharacters("ASDF", "ad") === "ASDF", "Test 2");
+    console.assert(removeCharacters("", "ad") === "", "Test 3");
+    console.assert(removeCharacters("asdf", "xz") === "asdf", "Test 4");
+    console.assert(removeCharacters("asdf", "") === "asdf", "Test 5");
+    console.assert(removeCharacters("howdy", "dw") === "hoy", "Test 6");
+    console.assert(removeCharacters("AaSsDdFf", "aSdF") === "AsDf", "Test 7");
+    console.assert(removeCharacters("asdf", "fdsa") === "", "Test 8");
   } catch (err) {
     console.error(err);
   }
